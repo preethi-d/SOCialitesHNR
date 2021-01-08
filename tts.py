d834571dc6e39284f3314e0d97d1d1ce21a2c599
yogtew <reachyogusvi@gmail.com>
2021-01-08 22:07:32 +0800

    Add language command

--- tts.py
+++ tts.py
@@ -1,10 +1,99 @@
 import logging
 
+from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove, Update
 from telegram import Update, InputFile
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, ConversationHandler, CallbackContext
 from gtts import gTTS
 
 token = "1590146552:AAHiUBLHNSWzknPRk0rxDV7Fsl5sl4GQuyU"
+
+raw_languages = """af: Afrikaans
+ar: Arabic
+bn: Bengali
+bs: Bosnian
+ca: Catalan
+cs: Czech
+cy: Welsh
+da: Danish
+de: German
+el: Greek
+en-au: English (Australia)
+en-ca: English (Canada)
+en-gb: English (UK)
+en-gh: English (Ghana)
+en-ie: English (Ireland)
+en-in: English (India)
+en-ng: English (Nigeria)
+en-nz: English (New Zealand)
+en-ph: English (Philippines)
+en-tz: English (Tanzania)
+en-uk: English (UK)
+en-us: English (US)
+en-za: English (South Africa)
+en: English
+eo: Esperanto
+es-es: Spanish (Spain)
+es-us: Spanish (United States)
+es: Spanish
+et: Estonian
+fi: Finnish
+fr-ca: French (Canada)
+fr-fr: French (France)
+fr: French
+gu: Gujarati
+hi: Hindi
+hr: Croatian
+hu: Hungarian
+hy: Armenian
+id: Indonesian
+is: Icelandic
+it: Italian
+ja: Japanese
+jw: Javanese
+km: Khmer
+kn: Kannada
+ko: Korean
+la: Latin
+lv: Latvian
+mk: Macedonian
+ml: Malayalam
+mr: Marathi
+my: Myanmar (Burmese)
+ne: Nepali
+nl: Dutch
+no: Norwegian
+pl: Polish
+pt-br: Portuguese (Brazil)
+pt-pt: Portuguese (Portugal)
+pt: Portuguese
+ro: Romanian
+ru: Russian
+si: Sinhala
+sk: Slovak
+sq: Albanian
+sr: Serbian
+su: Sundanese
+sv: Swedish
+sw: Swahili
+ta: Tamil
+te: Telugu
+th: Thai
+tl: Filipino
+tr: Turkish
+uk: Ukrainian
+ur: Urdu
+vi: Vietnamese
+zh-CN: Chinese
+zh-cn: Chinese (Mandarin/China)
+zh-tw: Chinese (Mandarin/Taiwan)"""
+
+languages_names = []
+languages = {}
+
+for line in raw_languages.split("\n"):
+    a, b = line.split(": ")
+    languages[b] = a
+    languages_names.append(b)
 
 # Enable logging
 logging.basicConfig(
@@ -12,6 +101,9 @@ logging.basicConfig(
 )
 
 logger = logging.getLogger(__name__)
+
+language = 'en'
+awaiting_choice = False
 
 
 # Define a few command handlers. These usually take the two arguments update and
@@ -26,14 +118,38 @@ def help_command(update: Update, context: CallbackContext) -> None:
     update.message.reply_text('Help!')
 
 
-def echo(update: Update, context: CallbackContext) -> None:
-    """Echo the user message."""
-    update.message.reply_text(update.message.text)
+def lang_command(update: Update, context: CallbackContext) -> int:
+    """Send a message when the command /lang is issues."""
+    reply_keyboard = [languages_names]
+    update.message.reply_text('Choose the language',
+                              reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
+    global awaiting_choice
+    awaiting_choice = True
+
+
+def set_language(update, new_lang):
+    if new_lang not in languages:
+        update.message.reply_text('Language {} not supported'.format(update.message.text),
+                                  reply_markup=ReplyKeyboardRemove())
+        return
+    logger.info("Language chosen: %s", new_lang)
+    update.message.reply_text('Language set to {}'.format(update.message.text),
+                              reply_markup=ReplyKeyboardRemove())
+    global language, awaiting_choice
+    language = languages[new_lang]
+    awaiting_choice = False
+
 
 def text_to_speech(update: Update, context: CallbackContext) -> None:
     """Convert the user message to speech and send the audio message."""
     print(update.message.text)
-    tts = gTTS(update.message.text)
+
+    global awaiting_choice
+
+    if awaiting_choice:
+        return set_language(update, update.message.text)
+
+    tts = gTTS(update.message.text, lang=language)
     tts.save("audio_message.ogg")
     update.message.reply_audio(open("audio_message.ogg", "rb"))
 
@@ -51,6 +167,8 @@ def main():
     # on different commands - answer in Telegram
     dispatcher.add_handler(CommandHandler("start", start))
     dispatcher.add_handler(CommandHandler("help", help_command))
+
+    dispatcher.add_handler(CommandHandler('lang', lang_command))
 
     # on noncommand i.e message - echo the message on Telegram
     dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, text_to_speech))
